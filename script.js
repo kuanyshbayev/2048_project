bf353dedd1d3f92596a86c8cff407ccad1779db4
Nurbol <kuanysbayev.nurbol@gmail.com>
2023-04-30 21:23:50 +0500

    движение ячеек

--- script.js
+++ script.js
@@ -1,5 +1,75 @@
 import { Grid } from "./grid.js";
+import { Tile } from "./tile.js";
 
 const gameBoard = document.getElementById("game-board");
 
 const grid = new Grid(gameBoard);
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+setupInputOnce();
+
+function setupInputOnce() {
+    window.addEventListener("keydown", handleInput, { once: true });
+  }
+function handleInput(event) {
+    switch (event.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+
+        case "ArrowDown":
+            break;
+
+        case "ArrowLefr":
+            break;
+
+        case "ArrowRight":
+            break;
+        
+        default:
+            setupInputOnce();
+            return;
+    }
+
+    setupInputOnce();
+    }
+
+    function moveUp() {
+        slideTiles(grid.cellsGroupedByColumn);
+    }
+
+    function slideTiles(groupedCells) {
+        groupedCells.forEach(group => slideTilesInGroup(group));
+    }
+
+    function slideTilesInGroup(group) {
+        for (let i=1; i<group.lenght; i++) {
+            if (group[i].isEmpty()){
+                continue;
+            }
+            const cellWithTile = group[i];
+
+            let targetCell;
+            let j = i - 1;
+            while (j >= 0 && group[j].canAccept(cellWithTile.linkedTile)) {
+                targetCell = group[j];
+                j--;
+            }
+
+            if (!targetCell) {
+                continue;
+              }
+            if (targetCell.isEmpty()) {
+                targetCell.linkTile(cellWithTile.linkedTile);
+            } else {
+                targetCell.linkedTileForMerge(cellWithTile.linkedTile);
+            }
+
+            cellWithTile.unlinkTile();
+        }
+    }
+
+
+
+
+
